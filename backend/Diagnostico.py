08c0716645aec80d254da48b5e24fc512a18955b
DavidRJimenez <david.jimenezn16@gmail.com>
2025-02-17 00:09:59 -0500

    Working Backend and TS

--- backend/Diagnostico.py
+++ backend/Diagnostico.py
@@ -2,9 +2,19 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from experta import Fact
 from MedicoSE import DrHouse, Sickness
+from fastapi.middleware.cors import CORSMiddleware
 
 # Inicializar la aplicación FastAPI
 app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos
+    allow_headers=["*"],  # Permite todos los headers
+)
 
 # Modelo de entrada para la API
 class SymptomsRequest(BaseModel):
